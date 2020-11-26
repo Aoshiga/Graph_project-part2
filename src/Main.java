7cea23b24f2472f28499c903faf33b715bd142b8
aoshiga <titouan.buronfosse@gmail.com>
2020-11-26 12:31:52 +0100

    Add Ressources folder and library

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,20 @@
 import maximumFlow.FlowNetwork;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 // added getEdge to get the instance of an edge in a graf from its source and target nodes
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        FlowNetwork flowNetwork = new FlowNetwork(new File("/test.dot"));
-        System.out.println(flowNetwork.toDotString());
+        try {
+            File f = new File("./Ressources/test.dot");
+            FlowNetwork flowNetwork = new FlowNetwork(f);
+            System.out.println(flowNetwork.toDotString());
+        } catch (FileNotFoundException e) {
+            System.out.println("Erreur : Aucun fichier trouvé");
+            System.out.println(e);
+        }
     }
 }
